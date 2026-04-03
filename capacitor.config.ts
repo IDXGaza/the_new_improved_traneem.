2645d2cb5cecdc36752a117380bdcab25cab914c
IDXGaza <idxgaza1122@gmail.com>
2026-04-04 01:58:19 +0300

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.idxgaza.traneem',
+  appName: 'ترانيم',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
